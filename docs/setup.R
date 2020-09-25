5915b31d606e33dc843ff871b80d525cb55aa794
Matthew T. Warkentin <matt.warkentin@mail.utoronto.ca>
2020-09-25 16:08:30 -0400

    first draft of incomplete pres

--- docs/setup.R
+++ docs/setup.R
@@ -10,13 +10,27 @@ knitr::opts_chunk$set(
   warning = FALSE
 )
 
+opaque_box <- function(...) {
+  htmltools::tags$div(
+    class = "bg-white-o90 shadow-3 h-75 pl3",
+    ...
+  )
+}
+
 library(metathis)
 
 htmltools::tagList(
+  xaringanExtra::use_clipboard(
+    button_text = "<i class=\"fa fa-clipboard\"></i>",
+    success_text = "<i class=\"fa fa-check\" style=\"color: #90BE6D\"></i>",
+    error_text = "<i class=\"fa fa-times-circle\" style=\"color: #F94144\"></i>"
+    ),
+  rmarkdown::html_dependency_font_awesome(),
   xaringanExtra::use_tachyons(minified = TRUE),
   xaringanExtra::use_webcam(width = 300, height = 300),
   xaringanExtra::use_animate_css(minified = TRUE, xaringan = FALSE),
   xaringanExtra::use_tile_view(),
+  xaringanExtra::use_broadcast(),
   xaringanExtra::use_extra_styles(
     hover_code_line = TRUE,
     mute_unhighlighted_code = TRUE
@@ -27,17 +41,17 @@ htmltools::tagList(
   ),
   meta() %>%
     meta_general(
-      description = "",
-      generator = ""
+      description = "Building impressive presentations using modern web technologies",
+      generator = "xaringan and remark.js"
     ) %>%
     meta_name(
       "github-repo" = "mattwarkentin/html-presentations-or-hiltswaltw"
       ) %>%
     meta_social(
-      title = "",
-      url = "",
+      title = "HTML Presentations or: How I Learned to Stop Worrying and Love the Web",
+      url = "https://mattwarkentin.github.io/html-presentations-or-hiltswaltw/",
       image = "",
-      image_alt = "",
+      image_alt = "Title slide for the talk titled HTML Presentations or: How I Learned to Stop Worrying and Love the Web",
       og_type = "website",
       og_author = "Matthew T. Warkentin",
       twitter_card_type = "summary_large_image",
